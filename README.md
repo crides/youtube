d82dd3699b42bb8f058f59cba11c0bed7f95e379
crides <zhuhaoqing@live.cn>
2021-07-07 12:26:04 -0500

    Update
    - add command to add external videos to the queue (most useful on
      customizable browsers like qutebrowser)
    - add `<c-r>` to reload the queue list in `fzf`
    - reworked subscribers format to include ranking

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Youtube Viewer
 
-This utility is meant to work as a simple Youtube play queue for myself. It can fetch from RSS feeds of subscribers, sync the current queue with Gist, and show a fzf interface for the actual playing.
+This utility is meant to work as a simple Youtube play queue for myself. It can fetch from RSS feeds of subscribers, and show a fzf interface for the actual playing.
+
+The subscribers format has been changed to use a custom json format, which is a list of objects like `{ "url": string, "name": string, rank: int}` where `rank` is how much you want to sort the results in fzf by.
--- youtube.py
+++ youtube.py
@@ -11,25 +11,26 @@ def dump_queue(Q):
 def read_queue():
     return json.load(open(QUEUE_FILE))
 
-def get_sub_urls():
-    from xml.etree import ElementTree
-    root = ElementTree.parse(f"{CONFIG_DIR}subscription_manager.xml").getroot()
-    return [sub.get("xmlUrl") for sub in root[0][0]]
+def get_subs():
+    import json
+    return json.load(open(f"{CONFIG_DIR}subs.json"))
 
-def get_vids_from_sub(url, from_time):
-    import feedparser, time
-    feed = feedparser.parse(url)
+def get_vids_from_sub(sub, from_time):
+    import feedparser, time, html
+    feed = feedparser.parse(sub["url"])
     new_vids = []
     for entry in feed.entries:
-        unix_time = time.mktime(entry["published_parsed"])
+        try:
+            unix_time = time.mktime(entry["published_parsed"])
+        except:
+            unix_time = time.time()
         if unix_time > from_time:
             new_vids.append({
                 "channel": entry["author"],
-                "title": entry["title"],
+                "title": html.unescape(entry["title"]),
                 "link": entry["link"],
                 "unix_time": unix_time,
             })
-    new_vids.sort(key=lambda v:v["unix_time"])
     return new_vids
 
 def get_duration(url):
@@ -38,12 +39,41 @@ def get_duration(url):
     body = request.urlopen(url).read().decode("utf8")
     lines = body.split("\n")
     for line in lines:
-        if 'itemprop="duration"' in line:
-            start = line.find("PT") + 2
-            end = line.find("S")
+        prop_ind = line.find('itemprop="duration"')
+        if prop_ind != -1:
+            start = line.find("PT", prop_ind) + 2
+            end = line.find("S", start)
             time = line[start:end].split("M")
             time = int(time[0]) * 60 + int(time[1])
             return time
+    return 0xAA
+
+def parse_time(s):
+    start = s.find("PT") + 2
+    end = s.find("S", start)
+    time = s[start:end].split("M")
+    time = int(time[0]) * 60 + int(time[1])
+    return time
+
+def get_info(url):
+    # Naive processing without BS
+    from urllib import request
+    import re, html
+    body = request.urlopen(url).read().decode("utf8")
+    lines = body.split("\n")
+    for line in lines:
+        prop_ind = line.find('itemprop="duration"')
+        if prop_ind != -1:
+            props = {m[0]: m[1] for m in re.findall(r"<meta itemprop=\"(.+?)\" content=\"(.+?)\">", line)}
+            channel = re.search(r"<link itemprop=\"name\" content=\"(.+?)\">", line).group(1)
+            duration = parse_time(props["duration"]) if "duration" in props else 0xAA
+            return {
+                "channel": channel,
+                "title": html.unescape(props["name"]),
+                "link": url,
+                "unix_time": time.time(),
+                "duration": duration,
+            }
 
 def id_from_url(url):
     id_ind = url.find("v=") + 2
@@ -52,10 +82,6 @@ def id_from_url(url):
 def thumbnail_path_from_url(url):
     id = id_from_url(url)
     return f"{CONFIG_DIR}{id}.jpg"
-
-def cache_video_path_from_url(url):
-    id = id_from_url(url)
-    return f"{CONFIG_DIR}{id}.mp4"
 
 def download_thumbnail(url):
     from urllib import request
@@ -68,101 +94,71 @@ def renew_queue(args):
     last_fetch = Q["fetch_time"] if "fetch_time" in Q else 0
     new_fetch = time.time() + time.timezone
     new_vids = []
-    sub_urls = get_sub_urls()
-    for i, sub_url in enumerate(sub_urls):
-        print(f"Fetching subscribers {i+1}/{len(sub_urls)}", end="\r")
-        new_vids.extend(get_vids_from_sub(sub_url, last_fetch))
+    subs = get_subs()
+    for i, sub in enumerate(subs):
+        print(f"Fetching subscribers {i+1}/{len(subs)}", end="\r")
+        new_vids.extend(get_vids_from_sub(sub, last_fetch))
     print()
     Q["fetch_time"] = new_fetch
     Q["videos"] = (Q["videos"] if "videos" in Q else []) + new_vids
     print(f"Added {len(new_vids)} videos to queue")
-    dump_queue(Q)
+    # dump_queue(Q)
     for i, video in enumerate(new_vids):
         print(f"Downloading thumbnails {i+1}/{len(new_vids)}", end="\r")
         link = video["link"]
         video["duration"] = get_duration(link)
-        dump_queue(Q)
-        download_thumbnail(link)
+        # dump_queue(Q)
+        # download_thumbnail(link)
+    ranks = {sub["name"]: sub["rank"] for sub in subs}
+    Q["videos"].sort(key=lambda v:(ranks.get(v["channel"], 100), v["unix_time"], v["channel"]))
+    Q["videos"].reverse()
+    dump_queue(Q)
     print()
 
-def push_queue(args):
-    from github import Github, InputFileContent
-    from getpass import getpass
-    queue_content = open(QUEUE_FILE).read()
-    Q = json.loads(queue_content)
-    username = input("Username: ")
-    passwd = getpass("Password: ")
-    g = Github(username, passwd)
-    user = g.get_user()
-    try:
-        if "gist_id" in Q:
-            gist_id = Q["gist_id"]
-            gist = g.get_gist(gist_id)
-            gist_files = {QUEUE_FILE: InputFileContent(queue_content, QUEUE_FILE)}
-            gist.edit("Youtube Queue", gist_files)
-        else:
-            gist_files = {QUEUE_FILE: InputFileContent("Nothing", QUEUE_FILE)}
-            gist = user.create_gist(False, gist_files, "Youtube Queue")
-            print(f"Created new gist: {gist_id}")
-            Q["gist_id"] = gist.id
-            queue_content = json.dumps(Q, indent=2)
-            dump_queue(Q)
-            gist_files = {QUEUE_FILE: InputFileContent(queue_content, QUEUE_FILE)}
-            gist.edit("Youtube Queue", gist_files)
-    except Exception as e:
-        print("Github error:", e)
-
-def pull_queue(args):
-    from getpass import getpass
-    from github import Github
+def add_vid(args):
+    link = args.link
     Q = read_queue()
-    username = input("Username: ")
-    passwd = getpass("Password: ")
-    g = Github(username, passwd)
-    user = g.get_user()
-    try:
-        if "gist_id" in Q:
-            gist_id = Q["gist_id"]
-            gist = g.get_gist(gist_id)
-            open(QUEUE_FILE, "w").write(gist.files[QUEUE_FILE].content)
-        else:
-            print("No Gist ID")
-    except Exception as e:
-        print("Github error:", e)
+    Q["videos"].insert(0, get_info(link))
+    dump_queue(Q)
 
 def list_videos():
     print("{:20}{:80}{}".format("Channel", "Title", "Time"))
     for vid in json.load(open(QUEUE_FILE))["videos"]:
         print("{:20}{:80}{}".format(vid["channel"], vid["title"], time.ctime(vid["unix_time"])))
 
+def get_entry_line(v, channel_width):
+    import time
+    duration = v["duration"]
+    return "\b".join([
+        f"{'W ' if 'watched' in v else '  '}{v['channel']:{channel_width}}{v['title']}",
+        v["link"],
+        v["channel"],
+        v["title"],
+        time.ctime(v["unix_time"]),
+        f"{duration//60:02}:{duration%60:02}",
+        f"Watched: {v['watched']//60:02}:{v['watched']%60:02}" if "watched" in v else "",
+    ])
+
+def fzf_get_lines():
+    Q = read_queue()
+    videos = Q["videos"]
+    channel_width = max(map(lambda v:len(v["channel"]), videos)) + 2
+    fzf_lines = [f"W {'Channel':{channel_width}}Title"]
+    fzf_lines.extend(get_entry_line(v, channel_width) for v in videos)
+    return "\n".join(fzf_lines)
+
+def fzf_get_lines_cmd(args):
+    print(fzf_get_lines())
+
 def play_queue(args):
     import textwrap, itertools, glob
     from subprocess import Popen, PIPE
     while True:
-        Q = read_queue()
-        videos = Q["videos"]
-        if len(videos) == 0:
-            print("No videos in queue.")
-            break
-        channel_width = max(map(lambda v:len(v["channel"]), videos)) + 2
-        fzf_lines = [f"W {'Channel':{channel_width}}Title"]
-        def get_entry_line(v):
-            import time
-            duration = v["duration"]
-            return "\b".join([
-                f"{'W ' if 'watched' in v else '  '}{v['channel']:{channel_width}}{v['title']}",
-                v["link"],
-                v["channel"],
-                v["title"],
-                time.ctime(v["unix_time"]),
-                f"{duration//60:02}:{duration%60:02}",
-                f"Watched: {v['watched']//60:02}:{v['watched']%60:02}" if "watched" in v else "",
-            ])
-        fzf_lines.extend(get_entry_line(v) for v in videos)
-        fzf_input = "\n".join(fzf_lines)
+        fzf_input = fzf_get_lines()
         fzf_binds = [
             ("ctrl-f", "page-down"),
             ("ctrl-b", "page-up"),
+            ("ctrl-r", "reload(youtube.py fzf-lines)"),
             ("?", "execute(less {f})"),
         ]
         # ueberzug_arg = "\\t".join(f"{t[0]}\\t{t[1]}" for t in [
@@ -184,7 +180,7 @@ def play_queue(args):
         ])
         fzf_opts = [
             "-e",   # --exact
-            "-s",
+            # "-s",
             # "--no-clear",
             "-m",   # --multi
             "--reverse",
@@ -208,11 +204,13 @@ def play_queue(args):
         for i, line in enumerate(results):
             results[i] = line[:-1].split(b"\b")[1].decode("utf8")
         if key == "del":
-            Q["videos"] = list(filter(lambda v:v["link"] not in results, videos))
+            Q = read_queue()
+            Q["videos"] = list(filter(lambda v:v["link"] not in results, Q["videos"]))
             dump_queue(Q)
-        elif key == "enter":
+        else:
             # os.system("tput rmcup")
-            Popen("mpv --fs --quiet " + " ".join(results), shell=True, stdout=None).wait()
+            print(f"Playing: {results[0]}")
+            Popen("mpv --quiet " + " ".join(results), shell=True, stdout=None).wait()
     # os.system("tput rmcup")
 
 def watched_video(args):
@@ -221,13 +219,11 @@ def watched_video(args):
     if args.finished:
         Q["videos"] = list(filter(lambda v:v["link"] != args.link, Q["videos"]))
         id = id_from_url(args.link)
-        print(args.link, id)
 
         for file in glob(f"{CONFIG_DIR}{id}.*"):
             os.remove(file)
         dump_queue(Q)
     else:
-        print("'" + args.time + "'")
         for i, video in enumerate(Q["videos"]):
             if video["link"] == args.link:
                 Q["videos"][i]["watched"] = int(float(args.time))
@@ -241,12 +237,11 @@ if __name__ == "__main__":
     sub_parsers = parser.add_subparsers()
     play_cmd = sub_parsers.add_parser("play")
     play_cmd.set_defaults(func=play_queue)
+    add_cmd = sub_parsers.add_parser("add")
+    add_cmd.add_argument("link")
+    add_cmd.set_defaults(func=add_vid)
     fetch_cmd = sub_parsers.add_parser("fetch")
     fetch_cmd.set_defaults(func=renew_queue)
-    push_cmd = sub_parsers.add_parser("push")
-    push_cmd.set_defaults(func=push_queue)
-    pull_cmd = sub_parsers.add_parser("pull")
-    pull_cmd.set_defaults(func=pull_queue)
     mpv_watch_cmd = sub_parsers.add_parser("mpv_watched")
     mpv_watch_cmd.set_defaults(func=watched_video, finished=True)
     mpv_watch_cmd.add_argument("link")
@@ -254,6 +249,8 @@ if __name__ == "__main__":
     mpv_Watch_cmd.set_defaults(func=watched_video, finished=False)
     mpv_Watch_cmd.add_argument("link")
     mpv_Watch_cmd.add_argument("time")
+    fzf_cmd = sub_parsers.add_parser("fzf-lines")
+    fzf_cmd.set_defaults(func=fzf_get_lines_cmd)
 
     match = parser.parse_args()
     match.func(match)
